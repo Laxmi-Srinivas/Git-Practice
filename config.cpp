c082fea8f58a26e3af4ebe51d4bb5ddac1917cb7
Srinivas <laxmi.srinivas.contact@gmail.com>
2024-11-14 11:02:19 +0530

    Version 1

---
+++ config.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"config"<<endl;
+    cout<<"version1"<<endl;
+}
---
+++ src/index.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"index"<<endl;
+    cout<<"version1"<<endl;
+}
